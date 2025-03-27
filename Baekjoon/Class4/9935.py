c292f7d91381a0a7513889d3cc0e21e28b8d3c9d
ehghks021203 <ehghks021203@gmail.com>
2025-03-28 04:20:12 +0900

    feat: 2025.03.28.

---
+++ Baekjoon/Class4/9935.py
@@ -0,0 +1,19 @@
+# 9935. 문자열 폭발
+
+def solution(str: str, boom: str) -> str:
+    stack = []
+    for s in str:
+        stack.append(s)
+        if len(stack) >= len(boom):
+            if "".join(stack[-len(boom):]) == boom:
+                del stack[-len(boom):]
+    if len(stack) == 0:
+        return "FRULA"
+    return "".join(stack)
+
+if __name__ == "__main__":
+    str = input()
+    boom = input()
+    print(solution(str, boom))
+
+# Result: 181932KB / 492ms
